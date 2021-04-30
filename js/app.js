52eb823cec1880ea8ea13aca9b66641bf2a37354
jgoldenbe <jgoldenbe@gmail.com>
2021-04-29 20:44:48 -0400

    updates to assignment 1

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ window.onload = () => {
   let state = document.getElementById("state");
   let zip = document.getElementById("zip");
   let salary = document.getElementById("salary");
-  let phoneNumber = document.getElementById("phoneNumber");
+  let phoneNumber = document.getElementById("phone-number");
   let availability = document.getElementById("availability");
   
   let national = document.getElementById("national");
@@ -23,39 +23,35 @@ window.onload = () => {
   let relocationOutput = document.getElementById("relocation-output");
 
   submit.addEventListener('click', () => {
-    if (firstName.value == null || lastName.value == null || street.value == null || city.value == null || state.value == null || zip.value == null || phoneNumber.value == null) {
-      alert('You are missing required fields. Please go back and fill in the form completely.');
+    if (firstName.value == null || lastName.value == null) {
+      fullNameOutput.textContent = `Name:`;
     } else {
-      if (firstName.value == null || lastName.value == null) {
-        fullNameOutput.textContent = `Name:`;
-      } else {
-        fullNameOutput.textContent = `Name: ${firstName.value} ${lastName.value}`;
-      }
-  
-      if (street == null || city == null || state == null || zip == null) {
-        addressOutput.textContent = null;
-      } else {
-        addressOutput.textContent = `Address: ${street.value} ${apt.value}, ${city.value}, ${state.value} ${zip.value}`;
-      }
-  
-      if (salary.value == '$') {
-        salaryOutput.textContent = `Desired Yearly Salary:`;
-      } else {
-        salaryOutput.textContent = `Desired Yearly Salary: ${salary.value}`;
-      }
-  
-      phoneOutput.textContent = `Phone Number: ${phoneNumber.value}`;
-      availabilityOutput.textContent = `Available Start Date: ${availability.value}`;
-  
-      if (national.checked == false && international.checked == false) {
-        relocationOutput.textContent = `Relocation: Not willing to relocate.`;
-      } else if (national.checked == true && international.checked == false) {
-        relocationOutput.textContent = `Relocation: Nationally`;
-      } else if (national.checked == false && international.checked == true) {
-        relocationOutput.textContent = `Relocation: Internationally`;
-      } else {
-        relocationOutput.textContent = `Relocation: Nationally and Internationally`;
-      }
+      fullNameOutput.textContent = `Name: ${firstName.value} ${lastName.value}`;
+    }
+
+    if (street == null || city == null || state == null || zip == null) {
+      addressOutput.textContent = null;
+    } else {
+      addressOutput.textContent = `Address: ${street.value} ${apt.value}, ${city.value}, ${state.value} ${zip.value}`;
+    }
+
+    if (salary.value == '$') {
+      salaryOutput.textContent = `Desired Yearly Salary:`;
+    } else {
+      salaryOutput.textContent = `Desired Yearly Salary: ${salary.value}`;
+    }
+
+    phoneOutput.textContent = `Phone Number: ${phoneNumber.value}`;
+    availabilityOutput.textContent = `Available Start Date: ${availability.value}`;
+
+    if (national.checked == false && international.checked == false) {
+      relocationOutput.textContent = `Relocation: Not willing to relocate.`;
+    } else if (national.checked == true && international.checked == false) {
+      relocationOutput.textContent = `Relocation: Nationally`;
+    } else if (national.checked == false && international.checked == true) {
+      relocationOutput.textContent = `Relocation: Internationally`;
+    } else {
+      relocationOutput.textContent = `Relocation: Nationally and Internationally`;
     }
   });
 };
